33aa35375e89e68c89e4d0dfc24b233251529af5
Johan Andersson <johan.rejeep@gmail.com>
2013-10-21 20:22:58 +0200

    Release v0.3.

--- epl.el
+++ epl.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
 ;;     Sebastian Wiesner <lunaryorn@gmail.com>
-;; Version: 0.3-cvs
+;; Version: 0.3
 ;; Package-Requires: ((cl-lib "0.3"))
 ;; Keywords: convenience
 ;; URL: http://github.com/cask/epl
