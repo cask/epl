a5c48952ed065c1afb9c56cd2cd7de46383d615b
Sebastian Wiesner <lunaryorn@gmail.com>
2013-11-01 12:05:27 +0100

    Bump version in master

--- epl.el
+++ epl.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastian Wiesner <lunaryorn@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
 ;;     Sebastian Wiesner <lunaryorn@gmail.com>
-;; Version: 0.4
+;; Version: 0.5-cvs
 ;; Package-Requires: ((cl-lib "0.3"))
 ;; Keywords: convenience
 ;; URL: http://github.com/cask/epl
